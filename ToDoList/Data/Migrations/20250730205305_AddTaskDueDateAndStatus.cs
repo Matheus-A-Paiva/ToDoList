cc7f9b3b3b227941c9a73ab0072348ca2cc1e5b4
Matheus Paiva <emanuelmatheus948@gmail.com>
2025-08-01 18:56:31 -0300

    feat: add default ASP.NET Core Identity scaffold

---
+++ ToDoList/Data/Migrations/20250730205305_AddTaskDueDateAndStatus.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoList.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTaskDueDateAndStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCompleted",
+                table: "Tasks");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DueDate",
+                table: "Tasks",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Tasks",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DueDate",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Tasks");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCompleted",
+                table: "Tasks",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
