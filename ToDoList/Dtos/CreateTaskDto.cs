ea4cd3d5621e5f3193c20c171a413d29c85795b9
Matheus Paiva <emanuelmatheus948@gmail.com>
2025-08-01 19:02:01 -0300

    feat: implement task DTOs

---
+++ ToDoList/Dtos/CreateTaskDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Dtos
+{
+    public class CreateTaskDto : IValidatableObject
+    {
+        [Required]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+        [Required]
+        public string Status { get; set; }
+        [Required]
+        public DateTime? DueDate { get; set; }
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (DueDate.HasValue && DueDate.Value.Date < DateTime.Now.Date)
+            {
+                yield return new ValidationResult(
+                    "Due date cannot be in the past.",
+                    new[] { nameof(DueDate) });
+            }
+        }
+    }
+}
---
+++ ToDoList/Dtos/UpdateTaskDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Dtos
+{
+    public class UpdateTaskDto
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+        [Required]
+        public string Status{ get; set; }
+        [Required]
+        public DateTime? DueDate { get; set; }       
+        
+    }
+}
